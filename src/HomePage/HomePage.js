c5435370c882ca50d6b41d1ab25b810188c4bd7a
Monesa <mthogulu@uncc.edu>
2020-10-12 17:27:15 -0400

    Brought Personal Budget React to git

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,114 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div id = "main" className="container center">
+
+        <div className="page-area">
+
+            <div className="text-box">
+               
+                <section>
+                <h1>Stay on track</h1>
+                <p>
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that! 
+                    Read more about how to <a href="sectionDetail.html">stay on track</a>
+                </p>
+            </section>
+            </div>
+
+            <div className="text-box">
+                <section>
+                <h1>Results</h1>
+                <p>
+                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                    Also, they to live happier lives... since they expend without guilt or fear... 
+                    because they know it is all good and accounted for.
+                    Read more about how to <a href="sectionDetail.html">generate Results</a>
+                </p>
+            </section>
+            </div>
+    
+            <div className="text-box">
+                <section>
+                <h1>Alerts</h1>
+                <p>
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                    Read more about how to <a href="sectionDetail.html">create Alerts</a>
+                </p>
+            </section>
+            </div>
+    
+            <div className="text-box">
+                <section>
+                <h1>Free</h1>
+                <p>
+                    This app is free!!! And you are the only one holding your data! Terms and Conditions apply.
+                    Read more about how to <a href="sectionDetail.html">Terms and Conditions</a>
+                </p>
+            </section>
+            </div>
+    
+            <div className="text-box">
+                <section>
+                <h1>Stay on track</h1>
+                <p>
+                    Do you know where you are spending your money? If you really stop to track it down,
+                    you would get surprised! Proper budget management depends on real data... and this
+                    app will help you with that!
+                    Read more about how to <a href="sectionDetail.html">stay on track</a>
+                </p>
+            </section>
+            </div>
+
+            <div className="text-box">
+                <section>
+                <h1>Results</h1>
+                <p>
+                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
+                    Also, they to live happier lives... since they expend without guilt or fear... 
+                    because they know it is all good and accounted for.
+                    Read more about how to <a href="sectionDetail.html">generate Results</a>
+                </p>
+            </section>
+            </div>
+    
+            <div className="text-box">
+                <section>
+                <h1>Alerts</h1>
+                <p>
+                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
+                    Read more about how to <a href="sectionDetail.html">create Alerts</a>
+                </p>
+            </section>
+            </div>
+    
+    
+            <div className="text-box">
+                <section>
+                <h1>Free</h1>
+                <p>
+                    This app is free!!! And you are the only one holding your data! Terms and Conditions apply.
+                    Read more about how to <a href="sectionDetail.html">Terms and Conditions</a>
+                </p>
+            </section>
+            </div>
+                
+            <div className="chart-container">
+                <h1>Chart Js Pie Chart</h1>
+                <canvas id="myChart"></canvas>
+
+                <h1>D3Js Chart</h1>
+                <div className="another-chart-container"></div>
+                </div>
+
+                
+        </div>
+        
+    </div>
+  );
+}
+
+export default HomePage;
